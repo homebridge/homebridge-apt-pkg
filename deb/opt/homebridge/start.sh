8d1015ffff559dd7a4e3429ddd590e16d8db756c
oz.nu <dev@oz.nu>
2022-06-10 10:10:49 +0000

    reload node_modules if ui reports any not found

--- deb/opt/homebridge/start.sh
+++ deb/opt/homebridge/start.sh
@@ -14,8 +14,23 @@ if [ -e $HB_SERVICE_STORAGE_PATH/package.json ]; then
   if [ "$?" != 0 ]; then
     echo "ERROR: $HB_SERVICE_STORAGE_PATH/package.json is not a valid JSON file; deleting..."
     rm -rf $HB_SERVICE_STORAGE_PATH/package.json
-    rm -rf $HB_SERVICE_STORAGE_PATH/node_modules/homebridge
-    rm -rf $HB_SERVICE_STORAGE_PATH/node_modules/homebridge-config-ui-x
+    rm -rf $HB_SERVICE_STORAGE_PATH/pnpm-lock.yaml
+    rm -rf $HB_SERVICE_STORAGE_PATH/node_modules
+  fi
+fi
+
+# if the ui failed to load due to a module being missing, re-install everything
+if [ -f $HB_SERVICE_STORAGE_PATH/.uix-module-not-found ]; then
+  echo "Homebridge UI reported it could not start because a module was missing, re-installing everything..."
+  echo "Re-installing Homebridge, Homebridge UI and all user plugins..."
+  rm -rf $HB_SERVICE_STORAGE_PATH/pnpm-lock.yaml
+  rm -rf $HB_SERVICE_STORAGE_PATH/node_modules
+  pnpm -C $HB_SERVICE_STORAGE_PATH install
+  if [ "$?" -eq 0 ]; then
+    rm -rf $HB_SERVICE_STORAGE_PATH/.uix-module-not-found
+    echo "Re-installation completed."
+  else
+    echo "Re-installation failed."
   fi
 fi
 

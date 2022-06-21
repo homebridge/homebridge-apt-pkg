64fe8d7eee2ee3f6759dafe4ccedbc1f1f013e40
oz.nu <dev@oz.nu>
2022-06-21 01:36:32 +0000

    remove ui check from start.sh

--- deb/opt/homebridge/start.sh
+++ deb/opt/homebridge/start.sh
@@ -19,12 +19,6 @@ if [ -e $HB_SERVICE_STORAGE_PATH/package.json ]; then
   fi
 fi
 
-# check for missing homebridge-config-ui-x
-if [ ! -f "$HB_SERVICE_EXEC_PATH" ]; then
-  echo "Re-installing homebridge-config-ui-x..."
-  pnpm -C $HB_SERVICE_STORAGE_PATH install --save homebridge-config-ui-x@latest
-fi
-
 # check for missing homebridge
 if [ ! -f "$HB_SERVICE_STORAGE_PATH/node_modules/homebridge/package.json" ]; then
   cd $HB_SERVICE_STORAGE_PATH
